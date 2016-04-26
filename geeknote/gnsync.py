870893ebf597b66324baa169b25909d02ecd1b57
Jin Xu <jinuxstyle@gmail.com>
2016-04-26 16:35:54 +0800

    Avoid update notes unnecessarilly and report note conflict
    Currently, gnsync uses mtime of the local and server notes to
    decide whether to update server or local notes. The problem is
    that by comparing the mtimes, gnsync can only know which note
    is newer than the other but not know whether they have been
    changed since the last sync, neither know whether they have
    conflicts or not. So it would update notes that have not been
    changed since the last sync unnecessarily, and it always
    overrite notes on server with the local notes if the notes
    on both side were changed.
    
    This patch saves the mtimes for both server and local notes to
    a file and it will be used as a guideline for future sync.
    By comparing current mtimes of a note with the saved mtimes,
    gnsync could know exactly whether the note on server was changed
    or the note was changed locally or the note on both side were
    changed. Thus, it can always make right decision to sync notes.
    
    If both local and server notes have been changed since last sync,
    gnsync will report the potential conflicts and suggest user to do
    merge. Upon conflicts, gnsync will download the server note and
    save it locally as a file prefixed with a dot and postfixed with
    a .rej extension. User could check the difference and do merge
    manually. After merged, user could sync the conflicting notebook
    again with a newly introduced option --merged to get the notebook
    in sync.

--- geeknote/gnsync.py
+++ geeknote/gnsync.py
@@ -8,6 +8,7 @@ import logging
 import string
 import unicodedata, re
 import sys
+import pickle
 
 from geeknote import GeekNote
 from storage import Storage
@@ -19,6 +20,9 @@ def_logpath = os.path.join(os.getenv('USERPROFILE') or os.getenv('HOME'),  'Geek
 formatter = logging.Formatter('%(asctime)-15s : %(message)s')
 handler = logging.FileHandler(def_logpath)
 handler.setFormatter(formatter)
+
+mtime_file_extention = '.mtime'
+rej_file_extension = '.rej'
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -85,12 +89,14 @@ class GNSync:
     mask = None
     sync_up = False
     sync_down = False
+    merged = None
 
     notebook_guid = None
     all_set = False
+    mtime_file = None
 
     @log
-    def __init__(self, notebook_name, path, mask, format, sync_mode):
+    def __init__(self, notebook_name, path, mask, format, sync_mode, merged=False):
         # check auth
         if not Storage().getUserToken():
             raise Exception("Auth error. There is not any oAuthToken.")
@@ -103,6 +109,9 @@ class GNSync:
             raise Exception("Path to sync directories does not exist.")
 
         self.path = path
+        self.mtime_file = os.path.join(self.path, '.' + notebook_name + mtime_file_extention)
+        if not os.path.exists(self.mtime_file):
+                open(self.mtime_file, 'wb') # create the mtime file
 
         #set mask
         if not mask:
@@ -129,6 +138,8 @@ class GNSync:
             self.sync_up = True
             self.sync_down = True
 
+        self.merged = merged
+
         logger.info('Sync Start')
 
         #set notebook
@@ -149,6 +160,13 @@ class GNSync:
         files = self._get_files()
         notes = self._get_notes()
 
+        mtimes = self._get_mtimes()
+        if mtimes is None:
+            mtimes = {}
+
+        #track files and notes changed in this sync.
+        updated = []
+
         if self.sync_up:
             logger.info('Uploading notes')
             for f in files:
@@ -156,12 +174,42 @@ class GNSync:
                 for n in notes:
                     if f['name'] == n.title:
                         has_note = True
-                        if f['mtime'] > n.updated:
-                            self._update_note(f, n)
-                            break
+                        #get saved mtime for the note
+                        mtime = mtimes.get(n.title, 0)
+                        if mtime != 0:
+                            if mtime['file_mtime'] == f['mtime'] and        \
+                                mtime['note_mtime'] == n.updated:
+                                #both have not changed since last sync
+                                break
+                            elif mtime['file_mtime'] != f['mtime'] and        \
+                                mtime['note_mtime'] != n.updated:
+                                #both changed, need merge
+                                if self.merged:
+                                    self._update_note(f, n)
+                                    updated.append(f['name'])
+                                else:
+                                    logger.warning('Skipped note (CONFLICT!!!): {0},\n'
+                                    'merge the note manually and sync the '
+                                    'notebook again with --merged option'.format(f['name']))
+                                    self._create_rej_file(n)
+                                break
+                            elif mtime['file_mtime'] != f['mtime']:
+                                #only local note changed
+                                self._update_note(f, n)
+                                updated.append(f['name'])
+                            else:
+                                #only server note changed, handle it in twoway mode
+                                pass
+                        else:
+                            if f['mtime'] > n.updated:
+                                self._update_note(f, n)
+                                updated.append(f['name'])
+
+                        break
 
                 if not has_note:
                     self._create_note(f)
+                    updated.append(f['name'])
 
         if self.sync_down:
             logger.info('Downloading notes')
@@ -169,13 +217,41 @@ class GNSync:
                 has_file = False
                 for f in files:
                     if f['name'] == n.title:
-                        has_file = True
-                        if f['mtime'] < n.updated:
-                            self._update_file(f, n)
+                            has_file = True
+                            mtime = mtimes.get(n.title, 0)
+                            if mtime != 0:
+                                if mtime['file_mtime'] == f['mtime'] and \
+                                    mtime['note_mtime'] == n.updated:
+                                    break
+                                elif mtime['file_mtime'] != f['mtime'] and \
+                                    mtime['note_mtime'] != n.updated:
+                                    break #handled already
+                                elif mtime['note_mtime'] != n.updated:
+                                    #only server note changed
+                                    self._update_file(f, n)
+                                    updated.append(n.title)
+                                else:
+                                    pass #handled already
+                            else:
+                                if f['mtime'] < n.updated:
+                                    self._update_file(f, n)
+                                    updated.append(n.title)
+
                             break
 
                 if not has_file:
                     self._create_file(n)
+                    updated.append(n.title)
+
+        # after sync, save the mtimes of both files and notes
+        files = self._get_files()
+        notes = self._get_notes()
+        for f in files:
+            for n in notes:
+                if f['name'] == n.title and n.title in updated:
+                    mtimes[n.title] = {'file_mtime':f['mtime'], 'note_mtime':n.updated}
+
+        self._save_mtimes(mtimes)
 
         logger.info('Sync Complete')
 
@@ -247,6 +323,19 @@ class GNSync:
         return True
 
     @log
+    def _create_rej_file(self, note):
+        """
+        Create reject file for note
+        """
+        GeekNote().loadNoteContent(note)
+        content = Editor.ENMLtoText(note.content)
+        rej_file = "." + note.title + self.extension + rej_file_extension
+        path = os.path.join(self.path, rej_file)
+        open(path, "w").write(content)
+        logger.info('Created reject file {0} for note: {1}'.format(rej_file, note.title))
+        return True
+
+    @log
     def _get_file_content(self, path):
         """
         Get file content.
@@ -256,7 +345,7 @@ class GNSync:
         # strip unprintable characters
         content = remove_control_characters(content.decode('utf-8')).encode('utf-8')
         content = Editor.textToENML(content=content, raise_ex=True, format=self.format)
-        
+
         if content is None:
             logger.warning("File {0}. Content must be " \
                            "an UTF-8 encode.".format(path))
@@ -322,6 +411,22 @@ class GNSync:
         keywords = 'notebook:"{0}"'.format(tools.strip(self.notebook_name))
         return GeekNote().findNotes(keywords, 10000).notes
 
+    @log
+    def _get_mtimes(self):
+        """
+        Get modification times
+        """
+        mtfile = open(self.mtime_file, 'rb')
+        mtimes = pickle.load(mtfile)
+        return mtimes
+
+    @log
+    def _save_mtimes(self, mtimes):
+        """
+        Save modification times
+        """
+        mtfile = open(self.mtime_file, 'wb')
+        pickle.dump(mtimes, mtfile)
 
 def main():
     try:
@@ -335,6 +440,7 @@ def main():
         parser.add_argument('--all', '-a', action='store_true', help='Synchronize all notebooks', default=False)
         parser.add_argument('--verbose', '-v', action='store_true', help='Verbose mode. Let GNSync print more logs about its progress')
         parser.add_argument('--one-way', '-w', action='store', default='up', choices=['up', 'down'], help='''One-way sync. Upload changed notes to server or download changes from server if "up" or "down" is given respectively. If --two-way option is specified, --one-way option will be ignored. If both are not given, GNSync will default to do one-way upload.''')
+        parser.add_argument('--merged', '-M', action='store_true', help='Update merged notes to server. Specify it only when conflicts are reported and you have resolved all of them manually on your local files')
 
         args = parser.parse_args()
 
@@ -345,6 +451,7 @@ def main():
         logpath = args.logpath if args.logpath else None
         twoway = True if args.two_way else False
         oneway = args.one_way if args.one_way else "up"
+        merged = True if args.merged else False
 
         sync_mode = "up" #default sync mode
         if twoway:
@@ -365,10 +472,10 @@ def main():
                 notebook_path = os.path.join(path, notebook)
                 if not os.path.exists(notebook_path):
                     os.mkdir(notebook_path)
-                GNS = GNSync(notebook, notebook_path, mask, format, sync_mode)
+                GNS = GNSync(notebook, notebook_path, mask, format, sync_mode, merged)
                 GNS.sync()
         else:
-            GNS = GNSync(notebook, path, mask, format, sync_mode)
+            GNS = GNSync(notebook, path, mask, format, sync_mode, merged)
             GNS.sync()
 
     except (KeyboardInterrupt, SystemExit, tools.ExitException):
